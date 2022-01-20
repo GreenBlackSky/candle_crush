2ac1ecb0309698815b8424b6278b74836a9c127e
GreenBlackSky <greenblacksky@gmail.com>
2022-01-20 20:27:31 +0300

    darkness added

--- scripts/Main.gd
+++ scripts/Main.gd
@@ -2,6 +2,4 @@ extends Node
 
 
 func _process(delta):
-	var player_position = $Player.transform.origin
-	for caster in $ShadowCasters.get_children():
-		caster.get_node('Shadow').look_at(player_position, Vector3.UP)
+	pass
