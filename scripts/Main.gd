3f25155ab6eb3e9a4df41e7a95ce90f47107b293
GreenBlackSky <greenblacksky@gmail.com>
2022-01-22 11:52:41 +0300

    basic UI

--- scripts/Main.gd
+++ scripts/Main.gd
@@ -1,5 +1,25 @@
 extends Node
 
+var paused = true
+
+signal pause
+signal unpause
 
 func _process(delta):
-	pass
+	if Input.is_action_pressed("ui_cancel"):
+		pause_game()
+
+func start_game():
+	$StartUI.hide()
+	paused = false
+	emit_signal("unpause")
+	
+func pause_game():
+	$ContinueUI.show()
+	paused = true
+	emit_signal("pause")
+	
+func continue_game():
+	$ContinueUI.hide()
+	paused = false
+	emit_signal("unpause")
--- scripts/Player.gd
+++ scripts/Player.gd
@@ -3,9 +3,13 @@ extends KinematicBody
 
 export var speed = 14
 var velocity = Vector3.ZERO
+var paused = true
 
 
 func _physics_process(delta):
+	if paused:
+		return
+
 	var direction = Vector3.ZERO
 
 	if Input.is_action_pressed("move_right"):
@@ -24,3 +28,10 @@ func _physics_process(delta):
 	velocity.x = direction.x * speed
 	velocity.z = direction.z * speed
 	velocity = move_and_slide(velocity, Vector3.UP)
+
+
+func start():
+	paused = false
+	
+func stop():
+	paused = true
